17b957f4e41aaeaa6822708bce6b13844eb9d1ad
baraa <baraa0ah@gmail.com>
2022-05-13 04:25:24 +0200

    added starter files

---
+++ server/routes/products.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+//router.METHOD(PATH, CONTROLLER_FUNCTION)
+//METHOD = GET,POST,PATCH,DELETE
+//PATH = String starting with /
+//CONTROLLER_FUNCTION = reference to the function that handles the logic for this specific route
+
+export default router;
