635a4b0e0bbd09bd60a60cc66a1c19931cb93fad
baraa <baraa0ah@gmail.com>
2022-05-16 21:04:07 +0200

    added hero to homepage

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,19 @@
+import styles from './home.module.css';
+import {Link} from "react-router-dom";
+import DeliveryImg from '../../shared/assets/delivery.png';
+
 const Home = () => {
     return (
-        <>
-            
-        </>
+        <div className={styles['wrapper']}>
+            <div className={styles['hero']}>
+                <h1>Groceries and more in <span>20</span> minutes</h1>
+                <p>Making on-demand retail a reality!</p>
+                <Link className={'btn1'} to={'products'}>Shop Now</Link>
+                <div className={styles['delivery']}>
+                    <img src={DeliveryImg} alt={'Delivery'} loading={'lazy'}/>
+                </div>
+            </div>
+        </div>
     );
 }
 
