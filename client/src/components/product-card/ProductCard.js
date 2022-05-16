9906c0642f68c87bc1d166bc606f7febf0cfb1a3
baraa <baraa0ah@gmail.com>
2022-05-16 23:46:33 +0200

    added product card

---
+++ client/src/components/product-card/ProductCard.js
@@ -0,0 +1,21 @@
+import styles from './productCard.module.css';
+
+const ProductCard = ({product}) => {
+    return (
+        <div className={styles['wrapper']}>
+            <div className={styles['image-wrapper']}>
+                <img src={product.image} alt={product.name}/>
+            </div>
+            <div className={styles['content']}>
+                <p className={styles['name']}>{product.name}</p>
+                <div className={styles['details']}>
+                    <p className={styles['weight']}>{product.weight}{product.measurement}</p>
+                    <p className={styles['price']}>{Number(product.price).toFixed(2)} EGP</p>
+                </div>
+                <div className={styles['add-to-cart']}>Add to Cart</div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
