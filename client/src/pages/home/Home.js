8d61a1783604176b22a5088a16065cedc6a874ea
baraa <baraa0ah@gmail.com>
2022-05-17 11:18:20 +0200

    added 5 categories to recommended products section

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -5,6 +5,7 @@ import Categories from "../../components/categories/Categories";
 import ProductCard from "../../components/product-card/ProductCard";
 
 const Home = () => {
+    const categories = ['Cold Drinks', 'Hot Drinks', 'Groceries', 'Fruits & Vegetables', 'Breakfast'];
     const getTestProduct = () => {
         return {
             id: 1,
@@ -29,15 +30,29 @@ const Home = () => {
                     <img src={DeliveryImg} alt={'Delivery'}/>
                 </div>
             </div>
-            <Categories/>
-            <div className={styles['recommended-wrapper']}>
+            <section>
+                <div className={'heading-wrapper'}>
+                    <h1 className={'heading'}>Categories</h1>
+                </div>
+                <Categories/>
+            </section>
+            <section>
                 <div className={'heading-wrapper'}>
                     <h1 className={'heading'}>Recommended Products</h1>
                 </div>
-                <div className={styles['products-wrapper']}>
-                    {products.map((product, i) => <ProductCard product={product} key={i}/>)}
+                <div className={styles['categories-wrapper']}>
+                    {categories.map((category, j) =>
+                        <div className={styles['category-wrapper']}>
+                            <div className={'heading2'}>
+                                <h2>{category}</h2>
+                            </div>
+                            <div className={styles['products-wrapper']}>
+                                {products.map((product, i) => <ProductCard product={product} key={i}/>)}
+                            </div>
+                        </div>
+                    )}
                 </div>
-            </div>
+            </section>
         </div>
     );
 }
