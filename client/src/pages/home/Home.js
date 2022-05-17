bcb235146e902974229f992ae7ff5903a4602dde
baraa <baraa0ah@gmail.com>
2022-05-17 18:01:37 +0200

    improved products styling

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -9,19 +9,64 @@ import PricesIcon from '../../shared/assets/why/prices.png';
 
 const Home = () => {
     const categories = ['Cold Drinks', 'Hot Drinks', 'Groceries', 'Fruits & Vegetables', 'Breakfast'];
-    const getTestProduct = () => {
-        return {
-            id: 1,
-            name: "Coca-Cola Can",
-            price: Math.random() * 1000,
-            image: "https://cdnprod.mafretailproxy.com/sys-master-root/hf4/h05/27691794989086/443516_main.jpg_480Wx480H",
-            weight: Math.floor(Math.random() * 900 + 100),
-            measurement: "ml",
-            category: "Cold Drinks"
-        }
-    }
 
-    const products = Array(5).fill(null).map(() => getTestProduct());
+    const products = [
+        {
+            "_id": {
+                "$oid": "6283c53abe9fa244b53d24f7"
+            },
+            "name": "Straberry Pack",
+            "price": 17,
+            "image": "https://fresh.hmart.com/media/catalog/product/cache/0c75b0d6b380af6dca3ec180c127a709/7/1/71575620002_1.png",
+            "weight": 250,
+            "measurement": "gm",
+            "category": "Fruits & Vegetables"
+        },
+        {
+            "_id": {
+                "$oid": "6283c53abe9fa244b53d24f8"
+            },
+            "name": "Imported Black Grapes",
+            "price": 44,
+            "image": "https://thefruteria.online/wp-content/uploads/2022/02/1642324866392.png",
+            "weight": 500,
+            "measurement": "gm",
+            "category": "Fruits & Vegetables"
+        },
+        {
+            "_id": {
+                "$oid": "6283c53abe9fa244b53d24f9"
+            },
+            "name": "Imported Sweet Pineapples",
+            "price": 90,
+            "image": "https://alchetron.com/cdn/pineapple-52c07283-1bf0-4b37-8f14-2c0f74bb44c-resize-750.png",
+            "weight": 1,
+            "measurement": "pc",
+            "category": "Fruits & Vegetables"
+        },
+        {
+            "_id": {
+                "$oid": "6283c53abe9fa244b53d24fa"
+            },
+            "name": "Imported Mixed Apples",
+            "price": 74,
+            "image": "https://www.sunmoonfood.com/wp-content/uploads/Fuji-Apples-02.png",
+            "weight": 6,
+            "measurement": "pcs",
+            "category": "Fruits & Vegetables"
+        },
+        {
+            "_id": {
+                "$oid": "6283c53abe9fa244b53d24fb"
+            },
+            "name": "Imported Yellow Apples",
+            "price": 70,
+            "image": "https://www.marlene.it/media/c7006c54-9054-4670-97ec-5b54b38ab8d2/602_x_602/p=5/golden-rgb-en.png",
+            "weight": 6,
+            "measurement": "pcs",
+            "category": "Fruits & Vegetables"
+        }
+    ]
 
     return (
         <div className={styles['wrapper']}>
