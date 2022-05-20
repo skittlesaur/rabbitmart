6840440d0ab383e12e4d260f2bacedba606e94e2
YoussefSaadGIU <youssef.elbasha.saad@gmial.com>
2022-05-20 21:59:20 +0200

    Added ordersRoutes to the server

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import orderRoutes from './routes/orders.js';
+
 const app = express();
 dotenv.config();
 
@@ -11,6 +13,7 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
+app.use('/orders', orderRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -21,6 +24,9 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log("Server listening on port " + PORT)))
-    .catch((error) => console.log(error.message));
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => app.listen(PORT, () => console.log("Server listening on port " + PORT)))
+.catch((error) => console.log(error.message));
