2cb6c7b1932c72f5b6052e695864fd2436b325e3
baraa <baraa0ah@gmail.com>
2022-05-21 21:55:46 +0200

    added redirection if user already logged in

--- client/src/pages/authentication/Authentication.js
+++ client/src/pages/authentication/Authentication.js
@@ -1,6 +1,18 @@
 import styles from './authentication.module.css';
+import {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
 const Authentication = ({data}) => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const profile = JSON.parse(localStorage.getItem('profile'));
+        const {user} = profile;
+
+        if (user)
+            navigate('/');
+    }, []);
 
     return (
         <div className={styles['wrapper']}>
