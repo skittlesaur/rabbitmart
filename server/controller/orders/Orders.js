a5fc4e915bd5673d8aaecc4b16f11bca8a030648
YoussefSaadGIU <youssef.elbasha.saad@gmial.com>
2022-05-23 12:51:58 +0200

    updated updateOrder and getOrder

--- server/controller/orders/Orders.js
+++ server/controller/orders/Orders.js
@@ -1,12 +1,12 @@
 import Order from "../../model/Orders.js";
 
-export const createOrder = async (req, res) => {
-
-}
-
 export const getOrder = async (req, res) => {
     try {
         const requiredOrder = await Order.find({ "order_id": req.params.id });
+
+        if(!requiredOrder.length){
+            return res.status(404).json({ message: "Order does not exist" });
+         }
 
         res.status(200).json(requiredOrder);
 
@@ -18,13 +18,20 @@ export const getOrder = async (req, res) => {
 
 export const updateOrder = async (req, res) => {
     try {
-        const requiredOrder = await Order.find({ "order_id": req.params.id });
 
         const status = req.body.status;
 
+        if(!['CREATED', 'PROCESSING', 'FULFILLED', 'CANCELLED'].includes(status)){
+            return res.status(400).json({ message: "Invalid status, has to be CREATED, PROCESSING, FULFILLED, CANCELLED" });
+        }
+
         const updatedOrder = await Order.findOneAndUpdate({ "order_id": req.params.id } , {
             orderStatus: status,
-        })
+        });
+
+        if(!updatedOrder){
+           return res.status(404).json({ message: "Order does not exist" });
+        }
 
         res.status(200).json(updatedOrder);
 
