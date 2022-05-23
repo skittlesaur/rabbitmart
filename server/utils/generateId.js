43b31a0150286d165b29a6cd7813ab0862e2f2b9
YoussefSaadGIU <youssef.elbasha.saad@gmial.com>
2022-05-23 18:29:07 +0200

    updated formatting

--- server/utils/generateId.js
+++ server/utils/generateId.js
@@ -1,6 +1,6 @@
 const generateId = () => {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < 6; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
