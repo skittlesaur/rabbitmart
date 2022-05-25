5d49fc626569bd2bd873f4ebe3f7760d6ced1d8a
baraa <baraa0ah@gmail.com>
2022-05-25 22:42:44 +0200

    removed warnings

--- server/utils/generateId.js
+++ server/utils/generateId.js
@@ -1,11 +1,11 @@
 const generateId = () => {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 6; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 6; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 export default generateId;
