007a570db31b547da672c857fa984f8846e7672d
YoussefSaadGIU <youssef.elbasha.saad@gmail.com>
2022-05-25 11:29:13 +0200

    getOrder now returns an object, updateOrder checks that the user is an admin first

--- server/controller/orders/Orders.js
+++ server/controller/orders/Orders.js
@@ -1,10 +1,11 @@
 import Order from "../../model/Orders.js";
+import Users from "../../model/Users.js";
 
 export const getOrder = async (req, res) => {
     try {
-        const requiredOrder = await Order.find({ "order_id": req.params.id });
+        const requiredOrder = await Order.findOne({ "order_id": req.params.id });
 
-        if(!requiredOrder.length){
+        if(!requiredOrder){
             return res.status(404).json({ message: "Order does not exist" });
          }
 
@@ -19,14 +20,26 @@ export const getOrder = async (req, res) => {
 export const updateOrder = async (req, res) => {
     try {
 
-        const status = req.body.status;
+        const orderStatus = req.body.status;
+        const id = req.body.id;
 
-        if(!['CREATED', 'PROCESSING', 'FULFILLED', 'CANCELLED'].includes(status)){
+        const user = await Users.findById(id, { password: 0 });
+
+        if(!user){
+            return res.status(404).json({ message: "User does not exist "});
+        }
+
+        if(!user.role == "ADMIN"){
+            res.status(401).json({ message: "Unauthorized user" })
+        }
+        
+
+        if(!['CREATED', 'PROCESSING', 'FULFILLED', 'CANCELLED'].includes(orderStatus)){
             return res.status(400).json({ message: "Invalid status, has to be CREATED, PROCESSING, FULFILLED, CANCELLED" });
         }
 
         const updatedOrder = await Order.findOneAndUpdate({ "order_id": req.params.id } , {
-            orderStatus: status,
+            order: orderStatus,
         });
 
         if(!updatedOrder){
