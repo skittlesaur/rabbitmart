c3765d96ae4b1b156159f89f79c4eba771cb9f96
YoussefSaadGIU <youssef.elbasha.saad@gmail.com>
2022-05-25 19:29:43 +0200

    added getAllOrders route

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { getOrder, updateOrder } from "../controller/orders/Orders.js";
+import { getOrder, getAllOrders, updateOrder } from "../controller/orders/Orders.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get('/:id', getOrder);
+router.get('/', auth, getAllOrders);
 router.patch('/:id', auth,  updateOrder);
 
 export default router;
