832b62583fcc25284c94198cc7810cfcc05c39b9
YoussefSaadGIU <youssef.elbasha.saad@gmail.com>
2022-05-25 11:29:35 +0200

    added auth middleware to updateOrder route

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { getOrder, updateOrder } from "../controller/orders/Orders.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get('/:id', getOrder);
-router.patch('/:id', updateOrder);
+router.patch('/:id', auth,  updateOrder);
 
 export default router;
