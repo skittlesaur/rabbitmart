24d6949690f04b1c1e57ecb9d75e3d0aff3e601d
baraa <baraa0ah@gmail.com>
2022-05-25 22:25:31 +0200

    fixed issue with end of the line

--- server/utils/CSVtoJSON.js
+++ server/utils/CSVtoJSON.js
@@ -1,13 +1,13 @@
 const convert = (csv) => {
     const lines = csv.toString().split("\n");
-    const headers = lines[0].split(',');
+    const headers = lines[0].split(',').map(h => h.replace(/\r/, ''));
 
     const result = [];
 
     for (let i = 1; i < lines.length; i++) {
         const line = lines[i];
         let obj = {};
-        const lineArray = line.split(",");
+        const lineArray = line.split(",").map(h => h.replace(/\r/, ''));
 
         for (const j in headers) {
             const header = headers[j];
