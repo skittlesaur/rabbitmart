b36a281a777ec8742993af2981584db379677d53
YoussefSaadGIU <youssef.elbasha.saad@gmail.com>
2022-06-03 18:03:58 +0200

    removed finalizeOrders

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,12 +1,11 @@
 import express from "express";
 
-import { createOrder, finalizeOrder, getOrder, getAllOrders, updateOrder } from "../controller/orders/Orders.js";
+import { createOrder, getOrder, getAllOrders, updateOrder } from "../controller/orders/Orders.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post('/', createOrder);
-router.post('/finalize', finalizeOrder);
 router.get('/:id', getOrder);
 router.get('/', auth, getAllOrders);
 router.patch('/:id', auth,  updateOrder);
