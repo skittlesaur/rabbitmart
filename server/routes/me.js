f54dae34c4924b1268920fa89c5867caa742ee99
baraa <baraa0ah@gmail.com>
2022-06-03 15:59:00 +0200

    added verify role endpoint

--- server/routes/me.js
+++ server/routes/me.js
@@ -1,12 +1,13 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import {login, verifyUser} from "../controller/me/Authentication.js";
+import {login, verifyRole, verifyUser} from "../controller/me/Authentication.js";
 import {getWishlist, updateWishlist} from "../controller/me/Wishlist.js";
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/verify', auth, verifyUser);
+router.post('/role', verifyRole)
 
 router.get('/wishlist', auth, getWishlist);
 router.patch('/wishlist', auth, updateWishlist);
