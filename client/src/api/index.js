49625a9f5dd8bf017b2649aaf7df383117265cf7
baraa <baraa0ah@gmail.com>
2022-06-03 16:08:30 +0200

    added base urls for each service

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import {ORDERS_BASEURL, PRODUCTS_BASEURL, SHIPPING_BASEURL, USER_BASEURL} from "./BaseURLs";
 
-const API = axios.create({baseURL: process.env.REACT_APP_API_BASEURL});
+const API = axios.create();
 
 API.interceptors.request.use((req) => {
     const profile = JSON.parse(localStorage.getItem('profile'));
@@ -11,21 +12,20 @@ API.interceptors.request.use((req) => {
     return req;
 });
 
-export const getRecommendations = () => API.get('/products/recommendations');
-export const postProduct = (product) => API.post(`/products`, product);
-export const validateCart = (cart) => API.post('/products/cart', {cart});
+export const getRecommendations = () => API.get(`${PRODUCTS_BASEURL}/recommendations`);
+export const postProduct = (product) => API.post(`${PRODUCTS_BASEURL}`, product);
+export const adminUpdateDatabase = (csv, mode) => API.patch(`${PRODUCTS_BASEURL}`, {csv, mode});
+export const validateCart = (cart) => API.post(`${PRODUCTS_BASEURL}/cart`, {cart});
 
-export const authLogin = (email, password) => API.post('/me/login', {email, password});
-export const verify = () => API.post('/me/verify');
-export const userUpdateWishlist = (product_id) => API.patch('/me/wishlist', {product_id});
-export const getWishlist = () => API.get('/me/wishlist');
+export const authLogin = (email, password) => API.post(`${USER_BASEURL}/login`, {email, password});
+export const verify = () => API.post(`${USER_BASEURL}/verify`);
+export const userUpdateWishlist = (product_id) => API.patch(`${USER_BASEURL}/wishlist`, {product_id});
+export const getWishlist = () => API.get(`${USER_BASEURL}/wishlist`);
 
-export const fetchShipments = (page) => API.get(`/shipping?page=${page}`);
-export const fetchShipment = (id) => API.get(`/shipping/${id}`);
-export const updateShipment = (id, status) => API.patch(`/shipping/${id}`, {status});
+export const fetchShipments = (page) => API.get(`${SHIPPING_BASEURL}?page=${page}`);
+export const fetchShipment = (id) => API.get(`${SHIPPING_BASEURL}/${id}`);
+export const updateShipment = (id, status) => API.patch(`${SHIPPING_BASEURL}/${id}`, {status});
 
-export const fetchOrders = (page) => API.get(`/orders?page=${page}`);
-export const fetchOrder = (id) => API.get(`/orders/${id}`);
-export const updateOrder = (id, status) => API.patch(`/orders/${id}`, {status});
-
-export const adminUpdateDatabase = (csv, mode) => API.patch(`/products`, {csv, mode});
+export const fetchOrders = (page) => API.get(`${ORDERS_BASEURL}?page=${page}`);
+export const fetchOrder = (id) => API.get(`${ORDERS_BASEURL}/${id}`);
+export const updateOrder = (id, status) => API.patch(`${ORDERS_BASEURL}/${id}`, {status});
