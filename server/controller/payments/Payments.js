3b52ce74968b337dc5642a7f83fa002944bdcd9a
YoussefSaadGIU <youssef.elbasha.saad@gmail.com>
2022-06-03 18:02:57 +0200

    removed handlePayment and implemented createCheckoutSession instead, changed webhook

--- server/controller/payments/Payments.js
+++ server/controller/payments/Payments.js
@@ -1,81 +1,79 @@
 import { stripe } from "../../app.js";
 import axios from "axios";
+import jwt from "jsonwebtoken";
 import generateId from "../../utils/generateId.js";
 
-export const handlePayment = async (req, res) => {
-    const { products, total, data } = req.body;
-    const order_id =  generateId();
-    //console.log(products, total, data);
-
-        try {        
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            mode: 'payment',
-            line_items: products.map( product => {
-                return {
-                    price_data: {
-                        currency: 'egp',
-                        product_data: {
-                            name: product.name
-                        },
-                        unit_amount: parseInt(product.price * 100)
-                    },
-                    quantity: product.quantity || 1
-                }
-
-            }),
-            metadata: {
-                order_id: order_id,
-                firstname: data.first,
-                lastname: data.last,
-                email: data.email,
-                phone_number: data.phone_number,
-             /* address: {
-                    country: data.country,
-                    city: data.city,
-                    area: data.area,
-                    street: data.street,
-                    building_number: data.building_number,
-                    floor: data.floor,
-                    apartment_number: data.apartment_number
-                }, */
-                total: total,
-                
+export const createCheckoutSession = async (req, res) => {
+  try {
+    const { products, total } = jwt.verify(
+        req.body.token,
+        process.env.JWT_SECRET_KEY
+      );
+      const { data } = req.body;
+      const order_id = generateId();
+    
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: products.map((product) => {
+        return {
+          price_data: {
+            currency: "egp",
+            product_data: {
+              name: product.name,
             },
-            success_url: "http://localhost:3000",
-            cancel_url:  "http://localhost:5000" 
-        });
-        
-        console.log({ session });
-        res.status(201).json({ url: session.url });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+            unit_amount: parseInt(product.price * 100),
+          },
+          quantity: product.quantity || 1,
+        };
+      }),
+      payment_intent_data: {
+        metadata: {
+          order_id: order_id,
+          firstName: data.name.first,
+          lastName: data.name.last,
+          email: data.email,
+          phone_number: data.phone_number,
+          address: JSON.stringify({
+            country: data.address.country,
+            city: data.address.city,
+            area: data.address.area,
+            street: data.address.street,
+            building_number: data.address.building_number,
+            floor: data.floor,
+            apartment_number: data.address.apartment_number,
+          }),
+          products: JSON.stringify(
+            products.map((product) => {
+              return {
+                product_id: product.product_id,
+                name: product.name,
+                quantity: product.quantity,
+              };
+            })
+          ),
+          total: total,
+        },
+      },
+      success_url: `http://localhost:3000/orders/${order_id}`,
+      cancel_url: "http://localhost:3000/cart",
+    });
+
+    res.status(201).json({ url: session.url });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const webhook = async (req, res) => {
-        const eventType = req.body.type;
-        const body = req.body;
-        const metadata = req.body.metadata;
-        console.log(metadata);
+  const eventType = req.body.type;
+  const { metadata } = req.body.data.object;
   try {
-    switch (eventType) {
-        case 'checkout.session.completed':
-            console.log(body);
-            console.log(body.metadata);
-            console.log("success!!!");
-          break;
-        case 'invoice.paid':
-          break;
-        case 'invoice.payment_failed':
-          break;
-        default:
-        // Unhandled event type
-      } 
-    
-      res.sendStatus(200);  
+    if (eventType === "charge.succeeded") {
+      await axios.post("http://localhost:5000/orders/", { data: metadata });
+    }
+    res.status(200).json(metadata);
   } catch (error) {
-      res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error.message });
   }
-    
-  };
+};
