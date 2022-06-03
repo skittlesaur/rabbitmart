fcca4de4a573cab91bde0be10085b52178cffc14
baraa <baraa0ah@gmail.com>
2022-06-03 19:00:57 +0200

    resolved conflicts

--- server/controller/orders/Orders.js
+++ server/controller/orders/Orders.js
@@ -1,60 +1,61 @@
-import axios from "axios";
-import jwt from "jsonwebtoken";
 import Order from "../../model/Orders.js";
 import Pagination from "../../utils/pagination.js";
-import generateId from "../../utils/generateId.js";
 import axios from "axios";
 import {USER_BASEURL, PRODUCTS_BASEURL, NOTIFICATIONS_BASEURL, SHIPPING_BASEURL} from "../../services/BaseURLs.js";
- 
+
 export const createOrder = async (req, res) => {
-  try {
-    const { data } = req.body;
-    const order = new Order({
-      order_id: data.order_id,
-      name: {
-        first: data.firstName,
-        last: data.lastName,
-      },
-      email: data.email,
-      phone_number: data.phone_number,
-      address: JSON.parse(data.address),
-      ordered_at: Date.now(),
-      products: JSON.parse(data.products),
-      total: data.total,
-    });
+    try {
+        const {data} = req.body;
+        const order = new Order({
+            order_id: data.order_id,
+            name: {
+                first: data.firstName,
+                last: data.lastName,
+            },
+            email: data.email,
+            phone_number: data.phone_number,
+            address: JSON.parse(data.address),
+            ordered_at: Date.now(),
+            products: JSON.parse(data.products),
+            total: data.total,
+        });
 
-    await order.save();
+        await order.save();
 
-    const deductQuantity = await axios.patch(
-      `${PRODUCTS_BASEURL}/updateQuantity`,
-      { products: order.products }
-    );
+        await axios.patch(
+            `${PRODUCTS_BASEURL}/updateQuantity`,
+            {products: order.products}
+        );
 
-    const to = order.email;
-    const notification = await axios.post(
-      `${NOTIFICATIONS_BASEURL}/order-confirmation`,
-      { to, order }
-    );
+        const to = order.email;
 
-    const ship = await axios.post(`${SHIPPING_BASEURL}`, {
-      ordered_at: order.ordered_at,
-      order_id: order.order_id,
-      address: order.address,
-      total: order.total,
-    });
+        await axios.post(
+            `${NOTIFICATIONS_BASEURL}/order-confirmation`,
+            {to, order}
+        );
 
-    res.status(200).json({ order_id: order.order_id });
-  } catch (error) {
-    res.status(404).json({ error: error.message });
-  }
+        await axios.post(`${SHIPPING_BASEURL}`, {
+            ordered_at: order.ordered_at,
+            order_id: order.order_id,
+            address: order.address,
+            total: order.total,
+        });
+
+        res.status(200).json({order_id: order.order_id});
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
 };
 
 export const getOrder = async (req, res) => {
-  try {
-    const requiredOrder = await Order.findOne({ order_id: req.params.id });
+    try {
+        const requiredOrder = await Order.findOne({order_id: req.params.id});
 
-    if (!requiredOrder) {
-      return res.status(404).json({ message: "Order does not exist" });
+        if (!requiredOrder) {
+            return res.status(404).json({message: "Order does not exist"});
+        }
+    } catch (error) {
+        res.status(400).json({message: error.message});
     }
 }
 
@@ -74,38 +75,16 @@ export const getAllOrders = async (req, res) => {
         const orders = await Order.find().sort({ordered_at: -1});
         const ordersPaged = Pagination(req.query.page, orders);
 
-    res.status(200).json(requiredOrder);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
+        const total_pages = Math.ceil((await Order.count()) / 20);
 
-export const getAllOrders = async (req, res) => {
-  try {
-    const id = req.body.id;
-    const user = await Users.findById(id, { password: 0 });
-
-    if (!user) {
-      return res.status(404).json({ message: "User does not exist " });
+        res.status(200).json({total_pages, orders: ordersPaged});
+    } catch (error) {
+        res.status(404).json({message: error.message});
     }
-
-    if (user.role !== "ADMIN") {
-      return res.status(401).json({ message: "Unauthorized user" });
-    }
-
-    const orders = await Order.find().sort({ ordered_at: -1 });
-    const ordersPaged = Pagination(req.query.page, orders);
-
-    const total_pages = Math.ceil((await Order.count()) / 20);
-
-    res.status(200).json({ total_pages, orders: ordersPaged });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
 };
 
 export const updateOrder = async (req, res) => {
-  try {
+    try {
 
         const orderStatus = req.body.status;
         const id = req.body.id;
