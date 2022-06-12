4427712e7c7addde90d7658e17d19130be38f0af
baraa <baraa0ah@gmail.com>
2022-06-12 11:43:14 +0200

    updated services base urls

--- server/services/BaseURLs.js
+++ server/services/BaseURLs.js
@@ -3,5 +3,5 @@ export const PRODUCTS_BASEURL = 'https://rabbitmart-products.vercel.app/products
 export const ORDERS_BASEURL = 'https://rabbitmart-orders.vercel.app/orders'
 export const SHIPPING_BASEURL = 'https://rabbitmart-shipping.vercel.app/shipping'
 export const NOTIFICATIONS_BASEURL = 'https://rabbitmart-notifications.vercel.app/notifications'
-export const PAYMENT_BASEURL = 'https://rabbitmart-payments.vercel.app/payment'
+export const PAYMENTS_BASEURL = 'https://rabbitmart-payments.vercel.app/payments'
 export const USER_BASEURL = 'https://rabbitmart-users.vercel.app/me'
