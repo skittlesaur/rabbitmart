9100d5ca6a1e3b5b596ac01dc842409cd158505c
baraa <baraa0ah@gmail.com>
2022-06-13 00:31:22 +0200

    improved redirection

--- client/src/pages/checkout/success.js
+++ client/src/pages/checkout/success.js
@@ -1,17 +1,28 @@
 import {useNavigate, useSearchParams} from "react-router-dom";
 import Loading from "../../components/loading/Loading";
 import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {fetchOrder} from "../../actions/orders";
 
 const Success = ({setCart}) => {
 
     const order_id = useSearchParams()[0].get('order');
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        setCart([]);
-        localStorage.removeItem('cart');
-        navigate(`/orders/${order_id}`);
-    }, [])
+        const onSuccess = () => {
+            navigate(`/orders/${order_id}`);
+            setCart([]);
+            localStorage.removeItem('cart');
+        }
+
+        const onError = () => {
+            window.location.reload();
+        }
+
+        dispatch(fetchOrder(order_id, onSuccess, onError));
+    }, [dispatch, navigate, order_id, setCart])
 
     return <Loading text={'Processing'}/>
 }
